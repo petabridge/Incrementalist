ef8884fa597ea34fc9968255c21387fd0495f069
Aaron Stannard <aaron@petabridge.com>
2019-03-07 15:27:16 -0600

    added Petabridge.Templates boilerplate

---
+++ src/Incrementalist.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Incrementalist.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
