84b92aa337a1b8904c8c773a8987d610d1f83fff
Aaron Stannard <aaron@petabridge.com>
2019-03-07 16:58:23 -0600

    Added initial console program

--- src/Incrementalist/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Incrementalist
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Incrementalist/IncrementalistBuildSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Incrementalist
+{
+    /// <summary>
+    /// The settings used for this execution of incremental build analysis.
+    /// </summary>
+    public class IncrementalistBuildSettings
+    {
+        public IncrementalistBuildSettings(string targetBranch, string solutionFile)
+        {
+            TargetBranch = targetBranch;
+            SolutionFile = solutionFile;
+        }
+
+        /// <summary>
+        /// The target branch to compare the current Git HEAD against,
+        /// i.e. the `dev` or `master` branch of the repository.
+        /// </summary>
+        public string TargetBranch { get; }
+
+        /// <summary>
+        /// The current solution file for us to analyze inside this repository.
+        /// </summary>
+        public string SolutionFile { get; }
+    }
+}
