3d76684a1b82c87704acd19893f90f1b22cf6d52
Aaron Stannard <aaron@petabridge.com>
2019-03-12 17:59:06 -0500

    added git diff detector

---
+++ src/Incrementalist/Git/DiffHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibGit2Sharp;
+
+namespace Incrementalist.Git
+{
+    /// <summary>
+    /// Generate diffs for the current repository between branches
+    /// </summary>
+    public static class DiffHelper
+    {
+        public static IEnumerable<string> ChangedFiles(Repository repo, string targetBranch)
+        {
+            return repo.Diff.Compare<TreeChanges>(repo.Branches[targetBranch].Tip.Tree, DiffTargets.Index).Select(x => x.Path);
+        }
+    }
+}
