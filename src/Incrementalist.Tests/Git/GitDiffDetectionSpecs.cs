4918557fb6f449ec5391eb05e7c9b339ca8fcbaa
Aaron Stannard <aaron@petabridge.com>
2019-03-13 17:17:37 -0500

    verified that GitDiff works as expected for adding files

---
+++ src/Incrementalist.Tests/Git/GitDiffDetectionSpecs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Incrementalist.Git;
+using Incrementalist.Tests.Helpers;
+using Xunit;
+
+namespace Incrementalist.Tests.Git
+{
+    public class GitDiffDetectionSpecs : IDisposable
+    {
+        public DisposableRepository Repository { get; }
+
+        public GitDiffDetectionSpecs()
+        {
+            Repository = new DisposableRepository();
+        }
+
+        [Fact(DisplayName = "Should detect files that have been added to git repo")]
+        public void Should_detect_added_files()
+        {
+            Repository.CreateBranch("foo").CheckoutBranch("foo").WriteFile("fuber.txt", "fuber").Commit("Fuberized file");
+            var diffedFiles = DiffHelper.ChangedFiles(Repository.Repository, "master").ToList();
+            diffedFiles.Count.Should().Be(1);
+            var file = diffedFiles[0];
+            Path.GetFileName(file).Should().Be("fuber.txt");
+        }
+
+        public void Dispose()
+        {
+            Repository?.Dispose();
+        }
+    }
+}
--- src/Incrementalist.Tests/Helpers/DisposableRepository.cs
+++ src/Incrementalist.Tests/Helpers/DisposableRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using LibGit2Sharp;
 
 namespace Incrementalist.Tests.Helpers
@@ -9,12 +10,17 @@ namespace Incrementalist.Tests.Helpers
     public class DisposableRepository : IDisposable
     {
         /// <summary>
+        /// Since it might take a few tries to delete the Git repository.
+        /// </summary>
+        private const int MaxDeleteAttempts = 5;
+
+        /// <summary>
         /// Needed to create repositories in random, temporary directories.
         /// </summary>
         /// <returns>The path to a temporary, random directory.</returns>
         public static string CreateTempDirectory()
         {
-            var dirPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetRandomFileName());
+            var dirPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Path.GetFileNameWithoutExtension(System.IO.Path.GetRandomFileName()));
             Directory.CreateDirectory(dirPath);
             return dirPath;
         }
@@ -31,6 +37,9 @@ namespace Incrementalist.Tests.Helpers
         {
             var repoPath = Repository.Init(BasePath);
             Repository = new Repository(repoPath);
+            var sig = CreateSignature();
+            Repository.Commit("First", sig, sig);
+            //Repository.CreateBranch("master"); // setup the master branch initially
         }
 
         public string BasePath { get; }
@@ -49,6 +58,18 @@ namespace Incrementalist.Tests.Helpers
         }
 
         /// <summary>
+        /// Checks out the specified branch, assuming it exists.
+        /// </summary>
+        /// <param name="branchName">The name of the branch to be checked out.</param>
+        /// <returns>The current <see cref="DisposableRepository"/>.</returns>
+        public DisposableRepository CheckoutBranch(string branchName)
+        {
+            var branch = Repository.Branches[branchName];
+            var currentBranch = Commands.Checkout(Repository, branch);
+            return this;
+        }
+
+        /// <summary>
         /// Add a new file to the repository.
         /// </summary>
         /// <param name="fileName">The name of the file to add or overwrite.</param>
@@ -58,7 +79,7 @@ namespace Incrementalist.Tests.Helpers
         {
             var filePath = Path.Combine(BasePath, fileName);
             File.WriteAllText(filePath, fileText);
-            Repository.Index.Add(filePath);
+            Repository.Index.Add(fileName);
             return this;
         }
 
@@ -75,7 +96,7 @@ namespace Incrementalist.Tests.Helpers
             return this;
         }
 
-        public static Signature CreateSignature(string name = null, string email = "")
+        public static Signature CreateSignature(string name = null, string email = null)
         {
             return new Signature(name ?? "Fuber", email ?? "fuber@petabridge.com", DateTimeOffset.UtcNow);
         }
@@ -83,7 +104,23 @@ namespace Incrementalist.Tests.Helpers
         public void Dispose()
         {
             Repository?.Dispose();
-            Directory.Delete(BasePath, true);
+            for (var attempt = 1; attempt <= MaxDeleteAttempts; attempt++)
+            {
+                try
+                {
+                    Directory.Delete(BasePath, true);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    if (attempt < MaxDeleteAttempts)
+                    {
+                        // some exponential backoff here
+                        Thread.Sleep(100 + (int)Math.Pow(10, attempt - 1));
+                    }
+                }
+            }
+           
         }
     }
 }
