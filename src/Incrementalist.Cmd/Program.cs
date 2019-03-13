11926a78c0b2512437538d73242c5b3946bec1db
Aaron Stannard <aaron@petabridge.com>
2019-03-12 19:23:20 -0500

    first working proof of concept

--- src/Incrementalist.Cmd/Program.cs
+++ src/Incrementalist.Cmd/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Incrementalist.FileSystem;
 using Incrementalist.Git;
 using LibGit2Sharp;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
 
 namespace Incrementalist.Cmd
 {
+    enum FileType
+    {
+        Code,
+        Project,
+        Solution,
+        Other
+    }
+
     class Program
     {
         private static string _originalTitle = null;
@@ -28,7 +41,7 @@ namespace Incrementalist.Cmd
                 Console.Title = _originalTitle; // reset the console window title back
         }
 
-        static int Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
             SetTitle();
 
@@ -49,21 +62,51 @@ namespace Incrementalist.Cmd
             var repoFolder = Repository.Discover(Directory.GetCurrentDirectory());
             var repository = new Repository(repoFolder);
             Console.WriteLine("Repo base is located in {0}", repoFolder);
+            var workingFolder = Directory.GetParent(repoFolder).Parent;
 
-            foreach(var file in DiffHelper.ChangedFiles(repository, "master"))
+            var affectedFiles = DiffHelper.ChangedFiles(repository, "master").Select(x => Path.GetFullPath(x, workingFolder.FullName)).ToList();
+            foreach (var file in affectedFiles)
                 Console.WriteLine("Modified file: {0}", file);
             //}
+
+            // Locate and register the default instance of MSBuild installed on this machine.
+            MSBuildLocator.RegisterDefaults();
+
+            var msBuild = MSBuildWorkspace.Create();
+            var progress = new Progress<ProjectLoadProgress>();
+            progress.ProgressChanged += ProgressOnProgressChanged;
+
             if (!string.IsNullOrEmpty(repoFolder))
             {
-                var workingFolder = Directory.GetParent(repoFolder).Parent;
+                
                 foreach (var sln in SolutionFinder.GetSolutions(workingFolder.FullName))
                 {
                     Console.WriteLine("Found solution file {0}", sln);
+
+                    var fullSln = await msBuild.OpenSolutionAsync(sln, progress);
+                    var allDocuments = fullSln.Projects.SelectMany(x => x.Documents)
+                        .GroupBy(x => x.FilePath, document => FileType.Code)
+                        .ToDictionary(x => Path.GetFullPath(x.Key, workingFolder.FullName), x => x.First())
+                        .Concat(fullSln.Projects.ToDictionary(x => Path.GetFullPath(x.FilePath, workingFolder.FullName), x => FileType.Project))
+                        .Concat(new Dictionary<string, FileType>{ { Path.GetFullPath(fullSln.FilePath, workingFolder.FullName), FileType.Solution } })
+                        .ToDictionary(x => x.Key, x => x.Value);
+
+                    Console.WriteLine("Checking to see if affected files are in solution...");
+                    foreach (var affectedFile in affectedFiles)
+                    {
+                        var fileInSln = allDocuments.ContainsKey(affectedFile);
+                        Console.WriteLine("{0} in solution? {1}", affectedFile, fileInSln);
+                    }
                 }
             }
 
             ResetTitle();
             return 0;
+        }
+
+        private static void ProgressOnProgressChanged(object sender, ProjectLoadProgress e)
+        {
+            Console.WriteLine("{0} {1} {2}", e.ElapsedTime, e.Operation, e.FilePath);
         }
     }
 }
