5ac89befbd049c917b6140c49bd867c69cc44c8a
Aaron Stannard <aaron@petabridge.com>
2019-05-01 15:31:02 -0500

    adding more task definitions

---
+++ src/Incrementalist/BuildCommandBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Incrementalist
+{
+    /// <summary>
+    /// Abstract base class with built-in logging support and templating
+    /// </summary>
+    public abstract class BuildCommandBase : IBuildCommand
+    {
+        protected BuildCommandBase(string name, ILogger logger, CancellationToken cancellationToken)
+        {
+            Name = name;
+            Logger = logger;
+            CancellationToken = cancellationToken;
+        }
+
+        protected ILogger Logger { get; }
+
+        protected CancellationToken CancellationToken { get; }
+
+        public string Name { get; }
+
+        public async Task<object> Process(Task<object> previousTask)
+        {
+            Logger.LogDebug("[{0}] - Entered Task", Name);
+
+            if (CancellationToken.IsCancellationRequested)
+            {
+                Logger.LogInformation("Cancellation requested. Terminating Incrementalist at stage [{0}]", Name);
+                CancellationToken.ThrowIfCancellationRequested();
+            }
+
+            try
+            {
+                return await ProcessImpl(previousTask);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "[{0}] - Catastrophic task failure.", Name);
+                throw;
+            }
+            finally
+            {
+                Logger.LogDebug("[{0}] - Exited Task", Name);
+            }
+        }
+
+        protected abstract Task<object> ProcessImpl(Task<object> previousTask);
+    }
+}
---
+++ src/Incrementalist/ProjectSystem/FindSolutionCmd.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Incrementalist.ProjectSystem
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Build task for finding a solution in the folder if none was specified on the CLI.
+    /// </summary>
+    public sealed class FindSolutionCmd : BuildCommandBase
+    {
+        private readonly string _folderPath;
+        private readonly string _searchFilter;
+        private readonly SearchOption? _searchOption;
+
+        public FindSolutionCmd(ILogger logger, string folderPath, CancellationToken token, 
+            string searchFilter = null, SearchOption? searchOption = null) : base("FindVsSolution", logger, token)
+        {
+            _folderPath = folderPath;
+            _searchFilter = searchFilter;
+            _searchOption = searchOption;
+        }
+
+        protected override async Task<object> ProcessImpl(Task<object> previousTask)
+        {
+            // previous task should be a no-op
+            await previousTask;
+
+            return SolutionFinder.GetSolutions(_folderPath, _searchFilter, _searchOption);
+        }
+    }
+}
