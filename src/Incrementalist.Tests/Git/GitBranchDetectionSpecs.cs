d7769f01df5059cb5ec801b4157b764099d7f9e0
Aaron Stannard <aaron@petabridge.com>
2019-05-03 13:35:34 -0500

    working on Git branch validation

---
+++ src/Incrementalist.Tests/Git/GitBranchDetectionSpecs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Incrementalist.Git;
+using Incrementalist.Tests.Helpers;
+using Xunit;
+
+namespace Incrementalist.Tests.Git
+{
+    public class GitBranchDetectionSpecs : IDisposable
+    {
+        public GitBranchDetectionSpecs()
+        {
+            Repository = new DisposableRepository();
+        }
+
+        public void Dispose()
+        {
+            Repository?.Dispose();
+        }
+
+        public DisposableRepository Repository { get; }
+
+        [Fact(DisplayName = "Should detect existing Git branch")]
+        public void ShouldDetectExistingBranch()
+        {
+            Repository.CreateBranch("foo");
+            DiffHelper.HasBranch(Repository.Repository, "foo").Should().BeTrue();
+        }
+    }
+}
