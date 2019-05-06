64d41feebd6812ad6a4d51ed3baa730db42cd5f7
Aaron Stannard <aaron@petabridge.com>
2019-05-06 13:12:57 -0500

    Git branch validation (#30)
    * Added branch validator
    
    * close #28

---
+++ src/Incrementalist.Tests/Git/GitBranchDetectionSpecs.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GitBranchDetectionSpecs.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Incrementalist.Git;
+using Incrementalist.Tests.Helpers;
+using Xunit;
+
+namespace Incrementalist.Tests.Git
+{
+    public class GitBranchDetectionSpecs : IDisposable
+    {
+        public GitBranchDetectionSpecs()
+        {
+            Repository = new DisposableRepository();
+        }
+
+        public void Dispose()
+        {
+            Repository?.Dispose();
+        }
+
+        public DisposableRepository Repository { get; }
+
+        [Fact(DisplayName = "Should detect existing Git branch")]
+        public void ShouldDetectExistingBranch()
+        {
+            Repository.CreateBranch("foo");
+            DiffHelper.HasBranch(Repository.Repository, "foo").Should().BeTrue();
+        }
+    }
+}
