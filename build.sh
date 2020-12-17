4c8b154e6c0152e3026b68a3bb0f9ff21c740d96
eabanonu <eabanonu@yahoo.com>
2020-12-17 15:12:48 +0100

    Install .NET Core on Linux

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.8.0/nuget.exe
 FAKE_VERSION=5.16.0
 FAKE_EXE=$TOOLS_DIR/FAKE/tools/FAKE.exe
 DOTNET_VERSION=5.0.101
+DOTNETCORE_VERSION=3.1.404
 DOTNET_INSTALLER_URL=https://dot.net/v1/dotnet-install.sh
 DOTNET_CHANNEL=LTS;
 DOCFX_VERSION=2.40.5
@@ -46,12 +47,21 @@ fi
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-echo "Installing .NET CLI..."
+echo "Installing .NET 5..."
 if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
   mkdir "$SCRIPT_DIR/.dotnet"
 fi
 curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
 bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --channel $DOTNET_CHANNEL --install-dir .dotnet --no-path
+export PATH="$SCRIPT_DIR/.dotnet":$PATH
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+chmod -R 0755 ".dotnet"
+"$SCRIPT_DIR/.dotnet/dotnet" --info
+
+echo "Installing .NET Core..."
+curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
+bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNETCORE_VERSION --channel $DOTNET_CHANNEL --install-dir .dotnet --no-path
 export PATH="$SCRIPT_DIR/.dotnet":$PATH
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
