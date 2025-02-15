f37b5692f584beecd6202fc5bc37314a1d26bf0a
Aaron Stannard <aaron@petabridge.com>
2025-02-15 13:40:14 -0600

    fix

--- src/Incrementalist.Cmd/Commands/RunDotNetCommandTask.cs
+++ src/Incrementalist.Cmd/Commands/RunDotNetCommandTask.cs
@@ -10,6 +10,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Incrementalist.Git;
 
 namespace Incrementalist.Cmd.Commands
 {
