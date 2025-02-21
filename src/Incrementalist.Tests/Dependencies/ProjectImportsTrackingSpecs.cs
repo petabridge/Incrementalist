861681ae90be5045eff0207f51ddac724ee213ff
Aaron Stannard <aaron@petabridge.com>
2025-02-21 12:21:33 -0600

    project imports tracking spec passes

--- src/Incrementalist.Tests/Dependencies/ProjectImportsTrackingSpecs.cs
+++ src/Incrementalist.Tests/Dependencies/ProjectImportsTrackingSpecs.cs
@@ -45,7 +45,9 @@ namespace Incrementalist.Tests.Dependencies
             var projectFile = new SlnFileWithPath(projectFilePath, new SlnFile(FileType.Project, ProjectId.CreateNewId())) ;
             var imports = ProjectImportsFinder.FindProjectImports(new[] { projectFile });
             
-            Assert.Equivalent(new ImportedFile(importedPropsFilePath, new[] { projectFile }.ToImmutableList()), imports.Values);
+            var expectedImport = new ImportedFile(importedPropsFilePath, new[] { projectFile }.ToImmutableList());
+            Assert.Collection(imports.Values, 
+                item => Assert.Equal(expectedImport.Path, item.Path));
         }
 
         [Fact(DisplayName = "When project imported file is changed, the project should be marked as affected")]
@@ -69,7 +71,8 @@ namespace Incrementalist.Tests.Dependencies
                 [projectFilePath] = new SlnFile(FileType.Project, ProjectId.CreateNewId())
             };
             var filteredAffectedFiles = await cmd.Process(Task.FromResult(solutionFiles));
-            Assert.Equivalent(projectFilePath, filteredAffectedFiles.Should().HaveCount(1).And.Subject.Keys);
+            Assert.Single(filteredAffectedFiles);
+            Assert.Equal(projectFilePath, filteredAffectedFiles.Keys.First());
         }
     }
 }
