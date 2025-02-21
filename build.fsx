bdb43a14bbd02d7ab9f67bc8b54baf4d980d7fc3
Aaron Stannard <aaron@petabridge.com>
2025-02-21 06:51:57 -0600

    fixed pre-release builds
    Only needs to use `RELEASE_NOTES.md` to get the information it needs

--- build.fsx
+++ build.fsx
@@ -24,14 +24,21 @@ let solutionFile = FindFirstMatchingFile "*.sln" __SOURCE_DIRECTORY__  // dynami
 let buildNumber = environVarOrDefault "BUILD_NUMBER" "0"
 let hasTeamCity = (not (buildNumber = "0")) // check if we have the TeamCity environment variable for build # set
 let preReleaseVersionSuffix = "beta" + (if (not (buildNumber = "0")) then (buildNumber) else DateTime.UtcNow.Ticks.ToString())
-let versionSuffix = 
-    match (getBuildParam "nugetprerelease") with
-    | "dev" -> preReleaseVersionSuffix
-    | _ -> ""
 
 let releaseNotes =
-    File.ReadLines "./RELEASE_NOTES.md"
+    File.ReadLines (__SOURCE_DIRECTORY__ @@ "RELEASE_NOTES.md")
     |> ReleaseNotesHelper.parseReleaseNotes
+
+let versionFromReleaseNotes =
+    match releaseNotes.SemVer.PreRelease with
+    | Some r -> r.Origin
+    | None -> ""
+
+let versionSuffix =
+    match (getBuildParam "nugetprerelease") with
+    | "dev" -> preReleaseVersionSuffix
+    | "" -> versionFromReleaseNotes
+    | str -> str
 
 // Directories
 let toolsDir = __SOURCE_DIRECTORY__ @@ "tools"
