ad6ff70de8af54e4142961f6604c7812fbdbdc52
Aaron Stannard <aaron@petabridge.com>
2025-04-16 10:18:05 -0500

    skip Windows path tests on non-Windows platforms

--- src/Incrementalist.Tests/GlobFilterTests.cs
+++ src/Incrementalist.Tests/GlobFilterTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Incrementalist.Cmd;
 using Xunit;
 
@@ -74,6 +75,12 @@ public class GlobFilterTests
             [InlineData(new[] { "SRC/**" }, 2)] // Case-insensitive check
             public void FilterProjects_OnlyTargetFilters_ReturnsMatchingProjects(string[] targetGlobs, int expectedCount)
             {
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    // skip
+                    return;
+                }
+                
                 // Arrange
                 var skipGlobs = Array.Empty<string>();
 
