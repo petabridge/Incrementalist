a9fece1648fdcd7679dc5b325f2d54197b938451
Aaron Stannard <aaron@petabridge.com>
2025-04-17 14:21:37 -0500

    Added reproduction for https://github.com/petabridge/Incrementalist/issues/406

--- src/Incrementalist.Tests/GlobFilterTests.cs
+++ src/Incrementalist.Tests/GlobFilterTests.cs
@@ -205,6 +205,14 @@ public class GlobFilterTests
             })]
         // Target tests, skip all -> Should be empty
         [InlineData(new[] { "tests/**" }, new[] { "**/*" }, new string[] { })]
+        [InlineData(new[] { "**/*ProjectA*", "**/samples/*.csproj" }, new[] { "tests/**" }, new
+            []
+            {
+                // should filter out the ProjectA tests, but keep ProjectA itself and the samples
+                "src/ProjectA/ProjectA.csproj",
+                "samples/Sample1/Sample1.csproj",
+                "samples/Sample2/Sample2.fsproj"
+            })]
         public void FilterProjects_BothFilters_AppliesTargetThenSkip(string[] targetGlobs, string[] skipGlobs,
             string[] expectedProjects)
         {
